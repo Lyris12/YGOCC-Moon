687773634a361ad5f1bcae5fa159d3af1c808cb2
Clara <lyris89@live.com>
2020-08-28 19:22:16 -0400

    Update "Fated HERO" cards
    Nerf:
    * "Charity"
    * "Despairful Light"
    * "Ritual"
    Fix:
    * "Chaos Deity" text: Her effect is OpT
    * "Desizer" script: ATK gain wore off after 2 turns

--- expansions/script/c102400196.lua
+++ expansions/script/c102400196.lua
@@ -3,7 +3,7 @@
 local cid,id=GetID()
 function cid.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON+CATEGORY_DECKDES)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
@@ -11,21 +11,28 @@ function cid.initial_effect(c)
 	e1:SetOperation(cid.activate)
 	c:RegisterEffect(e1)
 end
-function cid.filter0(c)
+function cid.filter1(c)
 	return c:IsType(TYPE_MONSTER) and c:IsCanBeFusionMaterial() and c:IsAbleToGrave()
-end
-function cid.filter1(c,e)
-	return not c:IsImmuneToEffect(e) and c:IsOnField()
 end
 function cid.filter2(c,e,tp,m,f,chkf)
 	return c:IsType(TYPE_FUSION) and c:IsSetCard(0xf7a) and (not f or f(c))
 		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,nil,chkf)
 end
+function cid.fcheck(tp,sg,fc)
+	return sg:FilterCount(Card.IsLocation,nil,LOCATION_DECK)<=1
+end
+function cid.gcheck(sg)
+	return sg:FilterCount(Card.IsLocation,nil,LOCATION_DECK)<=1
+end
 function cid.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		local chkf=tp
-		local mg1=Duel.GetFusionMaterial(tp):Filter(Card.IsOnField,nil)+Duel.GetMatchingGroup(cid.filter0,tp,LOCATION_DECK,0,nil)
+		local mg1=Duel.GetFusionMaterial(tp)+Duel.GetMatchingGroup(cid.filter1,tp,LOCATION_DECK,0,nil)
+		aux.FCheckAdditional=cid.fcheck
+		aux.GCheckAdditional=cid.gcheck
 		local res=Duel.IsExistingMatchingCard(cid.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,nil,chkf)
+		aux.FCheckAdditional=nil
+		aux.GCheckAdditional=nil
 		if not res then
 			local ce=Duel.GetChainMaterial(tp)
 			if ce~=nil then
@@ -41,8 +48,12 @@ function cid.target(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function cid.activate(e,tp,eg,ep,ev,re,r,rp)
 	local chkf=tp
-	local mg1=Duel.GetFusionMaterial(tp):Filter(cid.filter1,nil,e)+Duel.GetMatchingGroup(cid.filter0,tp,LOCATION_DECK,0,nil)
+	local mg1=Duel.GetFusionMaterial(tp):Filter(aux.NOT(Card.IsImmuneToEffect),nil,e)+Duel.GetMatchingGroup(cid.filter1,tp,LOCATION_DECK,0,nil)
+	aux.FCheckAdditional=cid.fcheck
+	aux.GCheckAdditional=cid.gcheck
 	local sg1=Duel.GetMatchingGroup(cid.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,chkf)
+	aux.FCheckAdditional=nil
+	aux.GCheckAdditional=nil
 	local mg2=nil
 	local sg2=nil
 	local ce=Duel.GetChainMaterial(tp)
@@ -59,7 +70,11 @@ function cid.activate(e,tp,eg,ep,ev,re,r,rp)
 		local tg=sg:Select(tp,1,1,nil)
 		local tc=tg:GetFirst()
 		if sg1:IsContains(tc) and (sg2==nil or not sg2:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
+			aux.FCheckAdditional=cid.fcheck
+			aux.GCheckAdditional=cid.gcheck
 			local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,nil,chkf)
+			aux.FCheckAdditional=nil
+			aux.GCheckAdditional=nil
 			tc:SetMaterial(mat1)
 			Duel.SendtoGrave(mat1,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
 			Duel.BreakEffect()
