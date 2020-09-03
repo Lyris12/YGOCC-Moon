394750ccaec24c37d91ac98aca3f61b30f21b70c
Clara <lyris89@live.com>
2020-09-03 11:28:33 -0400

    Fix more redtexts caused by lines 104 & 107 of "Ephesiam"

--- expansions/script/c19782405.lua
+++ expansions/script/c19782405.lua
@@ -101,10 +101,10 @@ function cid.aclimit1(e,re,tp)
 	return re:IsActiveType(TYPE_MONSTER) and aux.tgoval(e,re,tp)
 end
 function cid.aclimit2(e,re,tp)
-	return re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_SPELL) and aux.tgoval()
+	return re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_SPELL) and aux.tgoval(e,re,tp)
 end
 function cid.aclimit3(e,re,tp)
-	return re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_TRAP) and aux.tgoval()
+	return re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_TRAP) and aux.tgoval(e,re,tp)
 end
 function cid.imtg(e,c)
 	return c:IsSetCard(0xd7c)
