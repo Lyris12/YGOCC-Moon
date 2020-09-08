dbb2e74b914f7322c7d9b4abe4e3c507fb4c7d53
Clara <lyris89@live.com>
2020-09-08 14:02:11 -0400

    Nerf "False Reality Barrier" (again) by Lyris

--- expansions/script/c195208411.lua
+++ expansions/script/c195208411.lua
@@ -12,8 +12,10 @@ function cid.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_GRAVE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetCountLimit(1,id)
 	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
+	e2:SetCondition(function(e,tp) local ph=Duel.GetCurrentPhase() return (ph==PHASE_MAIN1 or ph==PHASE_MAIN2) and Duel.GetTurnPlayer()==tp end)
 	e2:SetCost(aux.bfgcost)
 	e2:SetTarget(cid.sptg)
 	e2:SetOperation(cid.spop)
@@ -33,18 +35,13 @@ end
 end
 	function cid.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(cid.filter,tp,LOCATION_REMOVED,0,1,nil)
-	and Duel.GetMatchingGroupCount(Card.IsAbleToDeck,tp,0,LOCATION_ONFIELD,nil)>=1 end
-	local g=Duel.GetMatchingGroup(Card.IsAbleToDeck,tp,0,LOCATION_ONFIELD,nil)
-	if g:GetCount()>0 then
-		Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
-	end
+	and Duel.IsExistingTarget(Card.IsAbleToDeck,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectTarget(Card.IsAbleToDeck,tp,0,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,#g,0,0)
 end
 	function cid.spop(e,tp,eg,ep,ev,re,r,rp)
 	if not Duel.IsExistingMatchingCard(cid.filter,tp,LOCATION_REMOVED,0,1,nil) then return false end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,0,LOCATION_ONFIELD,1,1,nil)
-	local tc=g:GetFirst()
-		if tc then
-		Duel.SendtoDeck(tc,nil,2,REASON_EFFECT)
-	end
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then Duel.SendtoDeck(tc,nil,2,REASON_EFFECT) end
 end
