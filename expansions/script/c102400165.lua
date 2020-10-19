bb97215b8933ee5a2f92a97483ee99e9d3ac862c
Lyris <lyris89@live.com>
2020-10-18 20:47:16 -0400

    Add "T.G. Scythe Snake" by Lyris

---
+++ expansions/script/c102400165.lua
@@ -0,0 +1,62 @@
+local cid,id=GetID()
+--T.G. Scythe Snake
+function cid.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCondition(function(e,tp) return Duel.GetTurnPlayer()~=tp end)
+	e1:SetTarget(cid.target)
+	e1:SetOperation(cid.operation)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetCountLimit(1,id)
+	e2:SetTarget(cid.thtg)
+	e2:SetOperation(cid.thop)
+	c:RegisterEffect(e2)
+end
+function cid.spfilter(c,e,tp)
+	return c:IsLevelBelow(4) and c:IsSetCard(0x27) and not c:IsCode(id)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function cid.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and cid.spfilter(chkc,e,tp) end
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+		and not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and Duel.IsExistingTarget(cid.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c+Duel.SelectTarget,2,0,0)
+end
+function cid.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) or not c:IsRelateToEffect(e)
+		or not tc or not tc:IsRelateToEffect(e) or Duel.SpecialSummon(c+tc,0,tp,tp,false,false,POS_FACEUP)<2 then return end
+	local sg=Duel.GetMatchingGroup(Card.IsSynchroSummonable,tp,LOCATION_EXTRA,0,1,nil,nil)
+	if #sg>0 and Duel.SelectYesNo(tp,1164) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		Duel.SynchroSummon(tp,sg:Select(tp,1,1,nil):GetFirst())
+	end
+end
+function cid.filter(c)
+	return c:IsSetCard(0x27) and not c:IsCode(id) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+function cid.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(cid.filter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function cid.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,cid.filter,tp,LOCATION_DECK,0,1,1,nil)
+	Duel.SendtoHand(g,nil,REASON_EFFECT)
+	Duel.ConfirmCards(1-tp,g)
+end
