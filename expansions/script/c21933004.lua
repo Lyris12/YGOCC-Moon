421bf7af64cc8908689476e737ffbb6032606871
XGlitchy30 <34371410+XGlitchy30@users.noreply.github.com>
2021-01-06 00:14:49 +0100

    Geargal change

--- expansions/script/c21933004.lua
+++ expansions/script/c21933004.lua
@@ -42,6 +42,10 @@ function cid.initial_effect(c)
 	e3:SetTarget(cid.shtg)
 	e3:SetOperation(cid.shop)
 	c:RegisterEffect(e3)
+	Duel.AddCustomActivityCounter(id,ACTIVITY_SPSUMMON,cid.counterfilter)
+end
+function cid.counterfilter(c)
+	return c:IsSetCard(0x72)
 end
 --SPSUMMON SELF
 function cid.cfilter(c)
@@ -59,9 +63,9 @@ end
 function cid.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsRelateToEffect(e) then 
-		if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 and Duel.IsExistingMatchingCard(Card.IsFacedown,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 and Duel.IsExistingMatchingCard(Card.IsFacedown,tp,LOCATION_MZONE,0,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
-			local g=Duel.SelectMatchingCard(tp,Card.IsFacedown,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+			local g=Duel.SelectMatchingCard(tp,Card.IsFacedown,tp,LOCATION_MZONE,0,1,1,nil)
 			if #g>=0 then
 				Duel.HintSelection(g)
 				Duel.ChangePosition(g,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK,POS_FACEUP_DEFENSE,POS_FACEUP_DEFENSE)
@@ -76,12 +80,9 @@ function cid.spop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetTarget(cid.splimit)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
-	local e2=e1:Clone()
-	e2:SetCode(EFFECT_CANNOT_SUMMON)
-	Duel.RegisterEffect(e2,tp)
 end
 function cid.splimit(e,c)
-	return c:IsLocation(LOCATION_EXTRA) and not c:IsType(TYPE_XYZ) and not c:IsRace(RACE_MACHINE)
+	return c:IsLocation(LOCATION_EXTRA) and not c:IsType(TYPE_XYZ)
 end
 
 --SPSUMMON
@@ -90,10 +91,21 @@ function cid.costfilter(c,e,tp)
 		and Duel.GetMZoneCount(tp,c)>0 and (c:IsLocation(LOCATION_HAND) or c:IsFaceup())
 end
 function cid.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckReleaseGroupEx(tp,cid.costfilter,1,nil,e,tp) end
+	if chk==0 then return Duel.CheckReleaseGroupEx(tp,cid.costfilter,1,e:GetHandler(),e,tp) and Duel.GetCustomActivityCount(id,tp,ACTIVITY_SPSUMMON)==0 end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(cid.sumlimit)
+	Duel.RegisterEffect(e1,tp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-	local g=Duel.SelectReleaseGroupEx(tp,cid.costfilter,1,1,nil,e,tp)
+	local g=Duel.SelectReleaseGroupEx(tp,cid.costfilter,1,1,e:GetHandler(),e,tp)
 	Duel.Release(g,REASON_COST)
+end
+function cid.sumlimit(e,c,sump,sumtype,sumpos,targetp,se)
+	return not cid.counterfilter(c)
 end
 function cid.spfilter(c,e,tp)
 	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x72) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
@@ -124,7 +136,7 @@ end
 
 --SEARCH
 function cid.filter(c,tp)
-	return c:IsType(TYPE_MONSTER) and c:IsRace(RACE_MACHINE) and c:IsLevel(3,4) and c:IsAbleToHand()
+	return c:IsType(TYPE_MONSTER) and c:IsRace(RACE_MACHINE) and c:IsLevel(3,4) and not c:IsCode(id) and c:IsAbleToHand()
 		and Duel.IsExistingMatchingCard(cid.attrfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,c:GetAttribute())
 end
 function cid.attrfilter(c,attr)
