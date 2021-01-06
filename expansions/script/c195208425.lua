6aa23c5d301ca042d582a0cd6f8859980391ca37
Lyris <lyris89@live.com>
2021-01-06 00:55:35 -0500

    Update "False Reality True God of Oblivion Dox"
    Is no longer immune to itself

--- expansions/script/c195208425.lua
+++ expansions/script/c195208425.lua
@@ -154,6 +154,7 @@ end
 end
 	function s.efilter(e,te)
 	local c=e:GetHandler()
+	if te:GetHandler()==c then return false end
 	if not te:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return true end
 	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
 	return not g:IsContains(c)
