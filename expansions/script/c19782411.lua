e4e15bf940e34a5c760ffbcf15791dcc001c1260
Lyris <lyris89@live.com>
2021-02-11 09:55:23 -0500

    Nerf "Rock Shades"
    Self-Summon is now HOpT and activated

--- expansions/script/c19782411.lua
+++ expansions/script/c19782411.lua
@@ -1,6 +1,6 @@
 --created by ZEN, coded by Lyris
-local cid,id=GetID()
-function cid.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:GLString(0)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -14,9 +14,9 @@ function cid.initial_effect(c)
 	e2:SetCountLimit(1,id)
 	e2:SetCategory(CATEGORY_TOHAND)
 	e2:SetCondition(function(e,tp,eg) return eg:IsExists(function(tc) return tc:GetSummonPlayer()~=tp end,1,nil) end)
-	e2:SetCost(cid.cost)
-	e2:SetTarget(cid.thtg)
-	e2:SetOperation(cid.thop)
+	e2:SetCost(s.cost)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
 	local e3=Effect.CreateEffect(c)
 	e3:GLString(2)
@@ -24,53 +24,53 @@ function cid.initial_effect(c)
 	e3:SetCode(EVENT_TO_GRAVE)
 	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e3:SetCategory(CATEGORY_TOHAND)
-	e3:SetCondition(cid.con)
-	e3:SetTarget(cid.target)
-	e3:SetOperation(cid.operation)
+	e3:SetCondition(s.con)
+	e3:SetTarget(s.target)
+	e3:SetOperation(s.operation)
 	c:RegisterEffect(e3)
 end
-function cid.cfilter(c)
+function s.cfilter(c)
 	return c:IsFaceup() and c:IsSetCard(0xd7c) and c:GetSequence()<5 and c:IsAbleToGraveAsCost()
 end
-function cid.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return Duel.IsExistingMatchingCard(cid.cfilter,tp,LOCATION_SZONE,0,1,c) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_SZONE,0,1,c) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,cid.cfilter,tp,LOCATION_SZONE,0,1,1,c)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_SZONE,0,1,1,c)
 	if #g>0 then
 		Duel.SendtoGrave(g,REASON_COST)
 	end
 end
-function cid.filter(c,tp)
+function s.filter(c,tp)
 	return c:GetSummonPlayer()~=tp
 end
-function cid.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=eg:Filter(cid.filter,nil,tp)
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=eg:Filter(s.filter,nil,tp)
 	if chk==0 then return #g>0 and g:IsExists(Card.IsAbleToHand,1,nil) end
 	Duel.SetTargetCard(eg)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,#g,0,0)
 end
-function cid.thop(e,tp,eg,ep,ev,re,r,rp)
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local g=eg:Filter(cid.filter,nil,tp):Filter(Card.IsRelateToEffect,nil,e)
+	local g=eg:Filter(s.filter,nil,tp):Filter(Card.IsRelateToEffect,nil,e)
 	if #g<0 then return end
 	Duel.SendtoHand(g,nil,REASON_EFFECT)
 end
-function cid.con(e,tp,eg,ep,ev,re,r,rp)
+function s.con(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsReason(REASON_COST) and e:GetHandler():IsPreviousLocation(LOCATION_SZONE) and e:GetHandler():GetPreviousSequence()<5 and re:IsHasType(0x7e0) and re:IsActiveType(TYPE_MONSTER)
 		and re:GetHandler():IsSetCard(0xd7c)
 end
-function cid.thfilter(c)
+function s.thfilter(c)
 	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsSetCard(0xd7c) and c:IsAbleToHand() and not c:IsCode(id)
 end
-function cid.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and cid.thfilter(chkc) end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.thfilter(chkc) end
 	if chk==0 then return true end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectTarget(tp,aux.NecroValleyFilter(cid.thfilter),tp,LOCATION_GRAVE,0,1,1,nil)
+	local g=Duel.SelectTarget(tp,aux.NecroValleyFilter(s.thfilter),tp,LOCATION_GRAVE,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,#g,0,0)
 end
-function cid.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) then Duel.SendtoHand(tc,nil,REASON_EFFECT) end
 end
--- expansions/script/c52401237.lua
+++ expansions/script/c52401237.lua
@@ -1,6 +1,6 @@
-local cid,id=GetID()
+local s,id=GetID()
 --Cosmicburst Dragon
-function cid.initial_effect(c)
+function s.initial_effect(c)
 	c:EnableReviveLimit()
 	aux.AddOrigBigbangType(c)
 	aux.AddBigbangProc(c,aux.FilterEqualFunction(Card.GetVibe,0),1,aux.NOT(aux.FilterEqualFunction(Card.GetVibe,0)),1)
@@ -8,7 +8,7 @@ function cid.initial_effect(c)
 	local e0=Effect.CreateEffect(c)
 	e0:SetType(EFFECT_TYPE_SINGLE)
 	e0:SetCode(EFFECT_MATERIAL_CHECK)
-	e0:SetValue(cid.matcheck)
+	e0:SetValue(s.matcheck)
 	c:RegisterEffect(e0)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
@@ -17,9 +17,9 @@ function cid.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetLabelObject(e0)
-	e1:SetCondition(cid.con1)
-	e1:SetTarget(cid.tg1)
-	e1:SetOperation(cid.op1)
+	e1:SetCondition(s.con1)
+	e1:SetTarget(s.tg1)
+	e1:SetOperation(s.op1)
 	c:RegisterEffect(e1)
 	--tribute then equip
 	local e2=Effect.CreateEffect(c)
@@ -29,9 +29,9 @@ function cid.initial_effect(c)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetCountLimit(1)
-	e2:SetCost(cid.cost2)
-	e2:SetTarget(cid.tg2)
-	e2:SetOperation(cid.op2)
+	e2:SetCost(s.cost2)
+	e2:SetTarget(s.tg2)
+	e2:SetOperation(s.op2)
 	c:RegisterEffect(e2)
 	--negate
 	local e3=Effect.CreateEffect(c)
@@ -42,9 +42,9 @@ function cid.initial_effect(c)
 	e3:SetCountLimit(1,EFFECT_COUNT_CODE_SINGLE)
 	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
 	e3:SetRange(LOCATION_MZONE)
-	e3:SetCondition(cid.discon)
-	e3:SetTarget(cid.distg)
-	e3:SetOperation(cid.disop)
+	e3:SetCondition(s.discon)
+	e3:SetTarget(s.distg)
+	e3:SetOperation(s.disop)
 	c:RegisterEffect(e3)
 	--destroy replace
 	local e4=Effect.CreateEffect(c)
@@ -52,29 +52,29 @@ function cid.initial_effect(c)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetCode(EFFECT_DESTROY_REPLACE)
 	e4:SetCountLimit(1,EFFECT_COUNT_CODE_SINGLE)
-	e4:SetTarget(cid.reptg)
-	e4:SetValue(function(e,tc) return cid.repfilter(tc,e:GetHandlerPlayer()) end)
-	e4:SetOperation(cid.repop)
+	e4:SetTarget(s.reptg)
+	e4:SetValue(function(e,tc) return s.repfilter(tc,e:GetHandlerPlayer()) end)
+	e4:SetOperation(s.repop)
 	c:RegisterEffect(e4)
 end
-function cid.matcheck(e,c)
+function s.matcheck(e,c)
 	e:SetLabel(c:GetMaterial():FilterCount(aux.FilterEqualFunction(Card.GetVibe,0),nil))
 end
-function cid.con1(e,tp,eg,ep,ev,re,r,rp)
+function s.con1(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_SPECIAL+340)
 end
-function cid.filter(c,tp)
+function s.filter(c,tp)
 	return c:IsType(TYPE_MONSTER) and c:CheckUniqueOnField(tp) and not c:IsForbidden()
 end
-function cid.tg1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and cid.filter(chkc,tp) end
+function s.tg1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.filter(chkc,tp) end
 	local mc=e:GetHandler():GetMaterialCount()-e:GetLabelObject():GetLabel()
-	if chk==0 then return mc>0 and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and Duel.IsExistingTarget(cid.filter,tp,LOCATION_GRAVE,0,1,nil,tp) end
+	if chk==0 then return mc>0 and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE,0,1,nil,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	local g=Duel.SelectTarget(tp,aux.NecroValleyFilter(cid.filter),tp,LOCATION_GRAVE,0,1,mc,nil,tp)
+	local g=Duel.SelectTarget(tp,aux.NecroValleyFilter(s.filter),tp,LOCATION_GRAVE,0,1,mc,nil,tp)
 	Duel.SetOperationInfo(0,CATEGORY_EQUIP,g,#g,0,0)
 end
-function cid.op1(e,tp,eg,ep,ev,re,r,rp)
+function s.op1(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsFaceup() or not c:IsRelateToEffect(e) or Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
 	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
@@ -88,32 +88,32 @@ function cid.op1(e,tp,eg,ep,ev,re,r,rp)
 			e1:SetCode(EFFECT_EQUIP_LIMIT)
 			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 			e1:SetLabelObject(c)
-			e1:SetValue(cid.eqlimit)
+			e1:SetValue(s.eqlimit)
 			tc:RegisterEffect(e1)
 		end
 		tc=sg:GetNext()
 	end
 end
-function cid.eqlimit(e,c)
+function s.eqlimit(e,c)
 	return e:GetOwner()==e:GetLabelObject()
 end
-function cid.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.CheckReleaseGroup(tp,Card.IsReleasable,1,e:GetHandler()) end
 	local g=Duel.SelectReleaseGroup(tp,Card.IsReleasable,1,1,e:GetHandler())
 	Duel.Release(g,REASON_COST)
 end
-function cid.eqfilter2(c,tp)
+function s.eqfilter2(c,tp)
 	return c:IsType(TYPE_MONSTER) and c:IsLevelBelow(4) and not c:IsForbidden() and c:CheckUniqueOnField(tp)
 end
-function cid.tg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and cid.eqfilter2(chkc,tp) end
+function s.tg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.eqfilter2(chkc,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-		and Duel.IsExistingTarget(cid.eqfilter2,tp,LOCATION_GRAVE,0,1,nil) end
+		and Duel.IsExistingTarget(s.eqfilter2,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	local g=Duel.SelectTarget(tp,cid.aux.NecroValleyFilter(eqfilter2),tp,LOCATION_GRAVE,0,1,1,nil)
+	local g=Duel.SelectTarget(tp,s.aux.NecroValleyFilter(eqfilter2),tp,LOCATION_GRAVE,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_EQUIP,g,#g,0,0)
 end
-function cid.op2(e,tp,eg,ep,ev,re,r,rp)
+function s.op2(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
@@ -124,14 +124,14 @@ function cid.op2(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetCode(EFFECT_EQUIP_LIMIT)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		e1:SetLabelObject(c)
-		e1:SetValue(cid.eqlimit)
+		e1:SetValue(s.eqlimit)
 		tc:RegisterEffect(e1)
 	end
 end
-function cid.cfilter(c,tp)
+function s.cfilter(c,tp)
 	return c:IsOnField() and c:IsControler(tp)
 end
-function cid.discon(e,tp,eg,ep,ev,re,r,rp)
+function s.discon(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) or not Duel.IsChainNegatable(ev) then return false end
 	if re:IsHasCategory(CATEGORY_NEGATE) and Duel.GetChainInfo(ev-1,CHAININFO_TRIGGERING_EFFECT):IsHasType(EFFECT_TYPE_ACTIVATE) then return false end
 	local ex1,tg1,tc1=Duel.GetOperationInfo(ev,CATEGORY_DESTROY)
@@ -141,15 +141,15 @@ function cid.discon(e,tp,eg,ep,ev,re,r,rp)
 	local ex5,tg5,tc5=Duel.GetOperationInfo(ev,CATEGORY_TOEXTRA)
 	local ex6,tg6,tc6=Duel.GetOperationInfo(ev,CATEGORY_TOGRAVE)
 	local ex7,tg7,tc7=Duel.GetOperationInfo(ev,CATEGORY_REMOVE)
-	return (ex1 and tg1~=nil and tc1+tg1:FilterCount(cid.cfilter,nil,tp)-#tg1>0)
-		or (ex2 and tg2~=nil and tc2+tg2:FilterCount(cid.cfilter,nil,tp)-#tg2>0)
-		or (ex3 and tg3~=nil and tc3+tg3:FilterCount(cid.cfilter,nil,tp)-#tg3>0)
-		or (ex4 and tg4~=nil and tc4+tg4:FilterCount(cid.cfilter,nil,tp)-#tg4>0)
-		or (ex5 and tg5~=nil and tc5+tg5:FilterCount(cid.cfilter,nil,tp)-#tg5>0)
-		or (ex6 and tg6~=nil and tc6+tg6:FilterCount(cid.cfilter,nil,tp)-#tg6>0)
-		or (ex7 and tg7~=nil and tc7+tg7:FilterCount(cid.cfilter,nil,tp)-#tg7>0)
+	return (ex1 and tg1~=nil and tc1+tg1:FilterCount(s.cfilter,nil,tp)-#tg1>0)
+		or (ex2 and tg2~=nil and tc2+tg2:FilterCount(s.cfilter,nil,tp)-#tg2>0)
+		or (ex3 and tg3~=nil and tc3+tg3:FilterCount(s.cfilter,nil,tp)-#tg3>0)
+		or (ex4 and tg4~=nil and tc4+tg4:FilterCount(s.cfilter,nil,tp)-#tg4>0)
+		or (ex5 and tg5~=nil and tc5+tg5:FilterCount(s.cfilter,nil,tp)-#tg5>0)
+		or (ex6 and tg6~=nil and tc6+tg6:FilterCount(s.cfilter,nil,tp)-#tg6>0)
+		or (ex7 and tg7~=nil and tc7+tg7:FilterCount(s.cfilter,nil,tp)-#tg7>0)
 end
-function cid.distg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return e:GetHandler():GetEquipCount()>0 end
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetHandler():GetEquipCount(),1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
@@ -157,7 +157,7 @@ function cid.distg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
 	end
 end
-function cid.disop(e,tp,eg,ep,ev,re,r,rp)
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=e:GetHandler():GetEquipGroup():Select(tp,1,1,nil)
 	if Duel.Destroy(g,REASON_EFFECT)~=0 then
@@ -166,25 +166,25 @@ function cid.disop(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 end
-function cid.repfilter(c,tp)
+function s.repfilter(c,tp)
 	return c:IsFaceup() and c:IsType(TYPE_MONSTER) and c:IsControler(tp) and c:IsLocation(LOCATION_ONFIELD) and c:IsReason(REASON_BATTLE) and not c:IsReason(REASON_REPLACE)
 end
-function cid.drepfilter(c,e)
+function s.drepfilter(c,e)
 	return c:IsDestructable(e) and not c:IsStatus(STATUS_DESTROY_CONFIRMED+STATUS_BATTLE_DESTROYED)
 end
-function cid.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return c:IsOnField() and c:IsFaceup()
-		and eg:IsExists(cid.repfilter,1,nil,tp) and e:GetHandler():GetEquipGroup():IsExists(cid.drepfilter,1,nil,e) end
+		and eg:IsExists(s.repfilter,1,nil,tp) and e:GetHandler():GetEquipGroup():IsExists(s.drepfilter,1,nil,e) end
 	if Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESREPLACE)
-		local g=e:GetHandler():GetEquipGroup():FilterSelect(tp,cid.drepfilter,1,1,nil)
+		local g=e:GetHandler():GetEquipGroup():FilterSelect(tp,s.drepfilter,1,1,nil)
 		e:SetLabelObject(g:GetFirst())
 		g:GetFirst():SetStatus(STATUS_DESTROY_CONFIRMED,true)
 		return true
 	else return false end
 end
-function cid.repop(e,tp,eg,ep,ev,re,r,rp)
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_CARD,0,id)
 	local tc=e:GetLabelObject()
 	tc:SetStatus(STATUS_DESTROY_CONFIRMED,false)
