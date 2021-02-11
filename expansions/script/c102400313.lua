95ba94510127f6e97540b1166c9b3af860bfef95
Lyris <lyris89@live.com>
2021-02-11 11:08:20 -0500

    Add cards by Lyris
    "Dark Tuner Raven Hood"
    "Raven of the Starless Night"

---
+++ expansions/script/c102400313.lua
@@ -0,0 +1,43 @@
+--created & coded by Lyris, art at https://i.pinimg.com/originals/d1/ba/09/d1ba09e4f3cdb0618f8cbdc4055e1b1a.jpg
+--DT―レイヴン・フード
+local s,id=GetID()
+function s.initial_effect(c)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e2:SetTarget(s.lvtg)
+	e2:SetOperation(s.lvop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_BE_SYNCHRO_MATERIAL)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetValue(s.synlimit)
+	c:RegisterEffect(e1)
+end
+function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsLevelAbove(1) end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsLevelAbove,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,nil,1) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,Card.IsLevelAbove,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,99,nil,1)
+end
+function s.lvop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	if not c:IsRelateToEffect(e) or c:IsFacedown() or not g then return end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_LEVEL)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetValue(g:Filter(Card.IsRelateToEffect,nil,e):GetSum(Card.GetLevel))
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	c:RegisterEffect(e1)
+end
+function s.synlimit(e,c)
+	if not c then return false end
+	return not c:IsSetCard(0x601)
+end
---
+++ expansions/script/c102400314.lua
@@ -0,0 +1,93 @@
+--created & coded by Lyris, art at https://arlenepowers.files.wordpress.com/2011/11/wipraven2.jpg
+--星なき夜のレイヴン
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_SPSUMMON_PROC)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e2:SetRange(LOCATION_EXTRA)
+	e2:SetCondition(s.sprcon)
+	e2:SetOperation(s.sprop)
+	e2:SetValue(SUMMON_TYPE_SYNCHRO)
+	c:RegisterEffect(e2)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetTarget(s.tg)
+	e1:SetOperation(s.op)
+	c:RegisterEffect(e1)
+end
+function s.mnfilter(c,g,lv)
+	return g:IsExists(s.mnfilter2,1,c,c,lv)
+end
+function s.mnfilter2(c,mc,lv)
+	return c:GetLevel()-mc:GetLevel()==lv
+end
+function s.fselect(g,tp,sc)
+	return g:GetCount()==2 and g:IsExists(function(c) return c:IsType(TYPE_TUNER) and c:IsSetCard(0x600) end,1,nil) and g:IsExists(aux.NOT(Card.IsType),1,nil,TYPE_TUNER) and g:IsExists(s.mnfilter,1,nil,g,-sc:GetLevel()) and Duel.GetLocationCountFromEx(tp,tp,g,sc)>0
+end
+function s.sprcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local g=Duel.GetMatchingGroup(aux.AND(Card.IsFaceup,Card.IsCanBeSynchroMaterial),tp,LOCATION_MZONE,0,nil,c)
+	return g:CheckSubGroup(s.fselect,2,2,tp,c)
+end
+function s.sprop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.GetMatchingGroup(aux.AND(Card.IsFaceup,Card.IsCanBeSynchroMaterial),tp,LOCATION_MZONE,0,nil,c)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local tg=g:SelectSubGroup(tp,s.fselect,false,2,2,tp,c)
+	c:SetMaterial(tg)
+	Duel.SendtoGrave(tg,REASON_MATERIAL+REASON_SYNCHRO)
+end
+function s.filter(c,e,tp)
+	return c:IsRace(RACE_WINDBEAST) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,0,1,ft,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,#g,0,0)
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	if ft<=0 or not g then return end
+	local sg=g:Filter(Card.IsRelateToEffect,nil,e)
+	if #sg>1 and Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then return end
+	if #sg>ft then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		sg=sg:Select(tp,ft,ft,nil)
+	end
+	local c=e:GetHandler()
+	for tc in aux.Next(sg) do if Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1,true)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		tc:RegisterEffect(e2,true)
+		local e3=Effect.CreateEffect(c)
+		e3:SetType(EFFECT_TYPE_SINGLE)
+		e3:SetCode(EFFECT_CANNOT_BE_FUSION_MATERIAL)
+		e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e3:SetValue(1)
+		tc:RegisterEffect(e3,true)
+		local e4=e3:Clone()
+		e4:SetCode(EFFECT_CANNOT_BE_XYZ_MATERIAL)
+		tc:RegisterEffect(e4,true)
+		local e5=e3:Clone()
+		e5:SetCode(EFFECT_CANNOT_BE_LINK_MATERIAL)
+		tc:RegisterEffect(e5,true)
+	end end
+	Duel.SpecialSummonComplete()
+end
