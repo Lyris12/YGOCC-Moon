1727d2572e7557fb66494b34a30d1fb3105622b5
Lyris <lyris89@live.com>
2021-07-07 17:21:32 -0400

    Update cards by Lyris
    Fix "Inlightened Eatos Wings" direct attacking effect:
    * Was continuous instead of activated
    * Was applying multiple times while it was on field
    Add "Rigil Rintaurus"

---
+++ expansions/script/c102400174.lua
@@ -0,0 +1,107 @@
+--created & coded by Lyris
+--リージル・リンタウルス
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_SPSUM_PARAM)
+	e1:SetTargetRange(POS_FACEUP_ATTACK,1)
+	e1:SetCondition(s.spcon)
+	c:RegisterEffect(e1)
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e5:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e5:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e5:SetOperation(s.retreg)
+	c:RegisterEffect(e5)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetCountLimit(1,id)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	return Duel.GetLocationCount(1-c:GetControler(),LOCATION_MZONE)>0
+end
+function s.retreg(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsSummonType(SUMMON_VALUE_SELF) then return end
+	c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET+RESET_PHASE+PHASE_END,0,2)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.retcon)
+	e1:SetOperation(s.retop)
+	e1:SetReset(RESET_PHASE+PHASE_END,2)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.retcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetOwner():GetFlagEffect(id)~=0
+end
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetOwner()
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_REMOVE_BRAINWASHING)
+	e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1:SetLabelObject(c)
+	e1:SetTarget(s.rettg)
+	Duel.RegisterEffect(e1,tp)
+	local e2=Effect.CreateEffect(e:GetHandler())
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_ADJUST)
+	e2:SetLabelObject(e1)
+	e2:SetOperation(s.reset)
+	Duel.RegisterEffect(e2,tp)
+end
+function s.rettg(e,c)
+	return c==e:GetLabelObject() and c:GetFlagEffect(id)~=0
+end
+function s.reset(e,tp,eg,ep,ev,re,r,rp)
+	e:GetLabelObject():Reset()
+	e:Reset()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	local p=c:GetOwner()
+	if chkc then return chkc:IsOnField() and not chkc:IsControler(p) and chkc~=c end
+	if chk==0 then return true end
+	Duel.Hint(HINT_SELECTMSG,p,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(p,nil,p,0,LOCATION_ONFIELD,1,1,c)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then Duel.Destroy(tc,REASON_EFFECT) end
+	local c=e:GetHandler()
+	if c:IsSummonType(SUMMON_VALUE_SELF) then
+		local e3=Effect.CreateEffect(c)
+		e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e3:SetCode(EVENT_CHAIN_SOLVING)
+		e3:SetCountLimit(1)
+		e3:SetCondition(s.negcon)
+		e3:SetOperation(s.negop)
+		e3:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e3,c:GetControler())
+	end
+end
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+	return rp==tp and re:IsActiveType(TYPE_SPELL)
+end
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,0,id)
+	Duel.NegateEffect(ev)
+end
