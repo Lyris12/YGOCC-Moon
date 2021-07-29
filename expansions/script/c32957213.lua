961b7ad7237d29caf67c6061780904b7cdd225c6
XGlitchy30 <34371410+XGlitchy30@users.noreply.github.com>
2021-07-29 18:06:59 +0200

    spiral drill fix

--- expansions/script/c32957213.lua
+++ expansions/script/c32957213.lua
@@ -10,47 +10,47 @@ function cid.initial_effect(c)
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_PRE_BATTLE_DAMAGE)
-	e2:SetCondition(cm.damcon)
-	e2:SetOperation(cm.damop)
+	e2:SetCondition(cid.damcon)
+	e2:SetOperation(cid.damop)
 	c:RegisterEffect(e2)
 	--spsummon
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(m,0))
+	e3:SetDescription(aux.Stringid(id,0))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_IGNITION)
 	e3:SetRange(LOCATION_HAND)
-	e3:SetCountLimit(1,m)
-	e3:SetCost(cm.cost)
-	e3:SetCondition(cm.spcon)
-	e3:SetTarget(cm.sptg)
-	e3:SetOperation(cm.spop)
+	e3:SetCountLimit(1,id)
+	e3:SetCost(cid.cost)
+	e3:SetCondition(cid.spcon)
+	e3:SetTarget(cid.sptg)
+	e3:SetOperation(cid.spop)
 	c:RegisterEffect(e3)
 	--search
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(m,1))
+	e4:SetDescription(aux.Stringid(id,1))
 	e4:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
 	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e4:SetProperty(EFFECT_FLAG_DELAY)
 	e4:SetCountLimit(1,31957213)
-	e4:SetCost(cm.cost)
-	e4:SetTarget(cm.srtg)
-	e4:SetOperation(cm.srop)
+	e4:SetCost(cid.cost)
+	e4:SetTarget(cid.srtg)
+	e4:SetOperation(cid.srop)
 	c:RegisterEffect(e4)
-	Duel.AddCustomActivityCounter(m,ACTIVITY_SPSUMMON,cm.counterfilter)
+	Duel.AddCustomActivityCounter(id,ACTIVITY_SPSUMMON,cid.counterfilter)
 end
-function cm.counterfilter(c)
+function cid.counterfilter(c)
 	return c:IsSetCard(0x205)
 end
-function cm.damcon(e,tp,eg,ep,ev,re,r,rp)
+function cid.damcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return ep~=tp and c==Duel.GetAttacker() and Duel.GetAttackTarget() and Duel.GetAttackTarget():IsDefensePos()
 end
-function cm.damop(e,tp,eg,ep,ev,re,r,rp)
+function cid.damop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.ChangeBattleDamage(ep,ev*2)
 end
-function cm.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetCustomActivityCount(m,tp,ACTIVITY_SPSUMMON)==0 end
+function cid.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetCustomActivityCount(id,tp,ACTIVITY_SPSUMMON)==0 end
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
@@ -58,35 +58,35 @@ function cm.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	e1:SetTargetRange(1,0)
 	e1:SetLabelObject(e)
-	e1:SetTarget(cm.splimit)
+	e1:SetTarget(cid.splimit)
 	Duel.RegisterEffect(e1,tp)
 end
-function cm.splimit(e,c,sump,sumtype,sumpos,targetp,se)
+function cid.splimit(e,c,sump,sumtype,sumpos,targetp,se)
 	return not c:IsSetCard(0x205)
 end
-function cm.spcon(e,tp,eg,ep,ev,re,r,rp)
+function cid.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0 and Duel.GetFieldGroupCount(1-tp,LOCATION_MZONE,0)>0
 end
-function cm.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function cid.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
-function cm.spop(e,tp,eg,ep,ev,re,r,rp)
+function cid.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 end
-function cm.srfilter(c)
+function cid.srfilter(c)
 	return c:IsCode(96212376,96212377,96212378) and c:IsAbleToHand()
 end
-function cm.srtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(cm.srfilter,tp,LOCATION_DECK,0,1,nil) end
+function cid.srtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(cid.srfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function cm.srop(e,tp,eg,ep,ev,re,r,rp)
+function cid.srop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,cm.srfilter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,cid.srfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
