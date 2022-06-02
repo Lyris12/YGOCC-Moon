e34e9e02dfdfb1f2ad5eb1920ed48363cfb2822d
XGlitchy30 <antonio.buglione2014@gmail.com>
2022-06-02 15:12:30 +0200

    WA Ascension change

--- expansions/script/c29493150.lua
+++ expansions/script/c29493150.lua
@@ -44,10 +44,12 @@ end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		local chkf=tp
+		local mg=Duel.GetFusionMaterial(tp)
 		local mg1=Duel.GetMatchingGroup(s.filter00,tp,LOCATION_HAND+LOCATION_ONFIELD,0,e:GetHandler())
 		local mg2=Duel.GetMatchingGroup(s.filter0,tp,LOCATION_GRAVE,0,nil)
-		mg1:Merge(mg2)
-		local res=Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,nil,chkf)
+		mg:Merge(mg1)
+		mg:Merge(mg2)
+		local res=Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg,nil,chkf)
 		if not res then
 			local ce=Duel.GetChainMaterial(tp)
 			if ce~=nil then
@@ -64,10 +66,12 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local chkf=tp
+	local mg=Duel.GetFusionMaterial(tp):Filter(s.filter1,nil,e)
 	local mg1=Duel.GetMatchingGroup(s.filter00,tp,LOCATION_HAND+LOCATION_ONFIELD,0,e:GetHandler()):Filter(s.filter1,nil,e)
 	local mg2=Duel.GetMatchingGroup(s.filter0,tp,LOCATION_GRAVE,0,nil):Filter(s.filter1,nil,e)
-	mg1:Merge(mg2)
-	local sg1=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,chkf)
+	mg:Merge(mg1)
+	mg:Merge(mg2)
+	local sg1=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg,nil,chkf)
 	local mg4=nil
 	local sg2=nil
 	local ce=Duel.GetChainMaterial(tp)
@@ -84,7 +88,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		local tg=sg:Select(tp,1,1,nil)
 		local tc=tg:GetFirst()
 		if sg1:IsContains(tc) and (sg2==nil or not sg2:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
-			local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,nil,chkf)
+			local mat1=Duel.SelectFusionMaterial(tp,tc,mg,nil,chkf)
 			tc:SetMaterial(mat1)
 			local mat2=mat1:Filter(Card.IsLocation,nil,LOCATION_GRAVE)
 			mat1:Sub(mat2)
