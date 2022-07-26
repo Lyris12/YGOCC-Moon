5b1818ac09e9f362655c13b76cd25db0f93a4a62
Lyris12 <lyris89@live.com>
2022-07-26 11:34:46 -0400

    remove manual mode autospawn

--- expansions/script/c102400295.lua
+++ expansions/script/c102400295.lua
@@ -2,18 +2,4 @@
 --半物質の腐敗
 local s,id,o=GetID()
 function s.initial_effect(c)
-	if not manual_global_check then
-		manual_global_check=true
-		local tp=c:GetControler()
-		local e0=Effect.CreateEffect(c)
-		e0:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e0:SetCode(EVENT_PHASE_START+PHASE_DRAW)
-		e0:SetCountLimit(1,5001+EFFECT_COUNT_CODE_DUEL)
-		e0:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-		e0:SetOperation(function()
-			local tk=Duel.CreateToken(tp,5000)
-			Duel.SendtoDeck(tk,nil,SEQ_DECKTOP,REASON_RULE)
-		end)
-		Duel.RegisterEffect(e0,tp)
-	end
 end
