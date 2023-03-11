53403e0f9d0b525931179e67d314bd50ed191901
XGlitchy30 <antonio.buglione2014@gmail.com>
2023-03-11 20:19:27 +0100

    Hyperdrive Mercenary fix

--- expansions/script/c29935989.lua
+++ expansions/script/c29935989.lua
@@ -38,7 +38,7 @@ end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToChain() and Duel.GetMZoneCount(tp)>0 then
-		Duel.SpecialSummonRedirect(e,c,0,tp,tp,false,false,POS_FACEUP)
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
 
