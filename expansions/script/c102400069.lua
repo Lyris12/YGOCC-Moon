f1d042b8f9330d524a36faf9fe6cfc5f3e2c941e
Lyris12 <Lyris89@live.com>
2023-04-17 16:18:21 -0400

    fix redtext

--- expansions/script/c102400069.lua
+++ expansions/script/c102400069.lua
@@ -78,7 +78,7 @@ function s.scheck(g,tp)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return false end
-	local mg=Duel.GetMatchingGroup(s.sfilter1,tp,LOCATION_GRAVE,0,1,nil)
+	local mg=Duel.GetMatchingGroup(s.sfilter1,tp,LOCATION_GRAVE,0,1,nil,e,tp)
 	if chk==0 then return Duel.IsPlayerCanSpecialSummonCount(tp,2)
 		and not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
 		and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
