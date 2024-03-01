846fc9eedcbac4a2efa92c63c2e1b3ce7fbf9527
Lyris12 <lyris89@live.com>
2024-03-01 16:34:26 -0500

    cleanup
    gapfilling
    unduplicating arts

--- expansions/script/c221594317.lua
+++ expansions/script/c221594317.lua
@@ -1,184 +1,157 @@
---created by Walrus, coded by Lyris
-local s,id=GetID()
+--[[
+Voidictator Rune - Soul of the Guardian
+Runa dei Vuotodespoti - Anima del Guardiano
+Card Author: Walrus
+Scripted by: XGlitchy30
+]]
+
+local s,id,o=GetID()
 function s.initial_effect(c)
-	aux.AddOrigPandemoniumType(c)
-	c:EnableReviveLimit()
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
-	e3:SetTargetRange(1,1)
-	e3:SetTarget(s.splimit)
-	c:RegisterEffect(e3)
-	local e6=Effect.CreateEffect(c)
-	e6:SetType(EFFECT_TYPE_QUICK_O)
-	e6:SetCode(EVENT_FREE_CHAIN)
-	e6:SetRange(LOCATION_SZONE)
-	e6:SetCountLimit(1)
-	e6:SetCategory(CATEGORY_RECOVER)
-	e6:SetCondition(aux.PandActCheck)
-	e6:SetCost(s.cost)
-	e6:SetTarget(s.target)
-	e6:SetOperation(s.operation)
-	c:RegisterEffect(e6)
-	local e7=Effect.CreateEffect(c)
-	e7:SetType(EFFECT_TYPE_QUICK_O)
-	e7:SetCode(EVENT_FREE_CHAIN)
-	e7:SetRange(LOCATION_SZONE)
-	e7:SetCountLimit(1,id)
-	e7:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e7:SetCondition(function(e) return aux.PandActCheck(e) and Duel.GetFieldGroupCount(e:GetHandlerPlayer(),LOCATION_MZONE,0)==0 end)
-	e7:SetCost(s.spcost)
-	e7:SetTarget(s.sptg)
-	e7:SetOperation(s.spop)
-	c:RegisterEffect(e7)
-	aux.EnablePandemoniumAttribute(c,e6,e7,true,TYPE_RITUAL+TYPE_EFFECT,aux.TRUE,s.actcost)
+	if not s.progressive_id then
+		s.progressive_id=id
+	else
+		s.progressive_id=s.progressive_id+1
+	end
+	c:SetUniqueOnField(1,0,id)
+	aux.AddCodeList(c,CARD_VOIDICTATOR_DEMON_GUARDIAN_OF_CORVUS)
+	--Activation
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCountLimit(1,id+100)
-	e1:SetCategory(CATEGORY_REMOVE)
-	e1:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_RITUAL) end)
-	e1:SetTarget(s.rgtg)
-	e1:SetOperation(s.rgop)
+	e1:Desc(0)
+	e1:SetCategory(CATEGORY_EQUIP)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET|EFFECT_FLAG_CONTINUOUS_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_EQUIP_LIMIT)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e2:SetValue(s.eqlimit)
+	c:RegisterEffect(e2)
+	--Up to thrice per turn, if your opponent Special Summons a Ritual Monster(s): Activate this effect; this card gains the effects of 1 of those Ritual Monsters until the end of the next turn.
+	aux.RegisterMergedDelayedEventGlitchy(c,s.progressive_id,EVENT_SPSUMMON_SUCCESS,s.cfilter,id,LOCATION_SZONE,nil,LOCATION_SZONE,nil,nil,true)
+	local e3=Effect.CreateEffect(c)
+	e3:Desc(1)
+	e3:SetType(EFFECT_TYPE_FIELD|EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_CUSTOM+s.progressive_id)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(3)
+	e3:SetFunctions(nil,nil,s.eftg,s.efop)
+	local ge=Effect.CreateEffect(c)
+	ge:SetType(EFFECT_TYPE_FIELD|EFFECT_TYPE_GRANT)
+	ge:SetRange(LOCATION_SZONE)
+	ge:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	ge:SetLabelObject(e3)
+	ge:SetCondition(s.grantcon)
+	ge:SetTarget(s.grantfilter)
+	c:RegisterEffect(ge)
+	--If this card battles, your opponent cannot activate cards or effects until the end of the Damage Step.
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_FIELD)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetCode(EFFECT_CHANGE_DAMAGE)
 	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e4:SetTargetRange(1,0)
-	e4:SetValue(s.rev)
-	c:RegisterEffect(e4)
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e2:SetCategory(CATEGORY_REMOVE)
-	e2:SetCondition(function(e) return Duel.GetAttackTarget() and e:GetHandler():IsStatus(STATUS_OPPO_BATTLE) end)
-	e2:SetTarget(s.rdtg)
-	e2:SetOperation(s.rdop)
-	c:RegisterEffect(e2)
+	e4:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetTargetRange(0,1)
+	e4:SetValue(1)
+	e4:SetCondition(s.actcon)
+	local ge2=ge:Clone()
+	ge2:SetLabelObject(e4)
+	c:RegisterEffect(ge2)
+	--If this card battles an opponent's monster, it gains ATK equal to the ATK or DEF (whichever is higher, or its ATK, if tied) of that monster during damage calculation only.
 	local e5=Effect.CreateEffect(c)
-	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e5:SetCode(EVENT_REMOVE)
-	e5:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
-	e5:SetCategory(CATEGORY_TOEXTRA)
-	e3:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) local tc=e:GetHandler() return re and re:GetHandler():IsSetCard(0xc97) and re:GetHandler()~=tc and tc:IsReason(REASON_EFFECT) end)
-	e5:SetTarget(s.tg)
-	e5:SetOperation(s.op)
-	c:RegisterEffect(e5)
+	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e5:SetCode(EFFECT_UPDATE_ATTACK)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCondition(s.atkcon)
+	e5:SetValue(s.atkval)
+	local ge3=ge:Clone()
+	ge3:SetLabelObject(e5)
+	c:RegisterEffect(ge3)
 end
-function s.splimit(e,c,sump,sumtype,sumpos,targetp)
-	return sumtype&SUMMON_TYPE_PANDEMONIUM==SUMMON_TYPE_PANDEMONIUM
+--E1
+function s.filter(c)
+	if not c:IsFaceup() then return false end
+	return c:IsCode(CARD_VOIDICTATOR_DEMON_GUARDIAN_OF_CORVUS)
 end
-function s.filter(c,sp)
-	return c:GetSummonPlayer()==sp and c:GetSummonLocation()==LOCATION_EXTRA
-end
-function s.rfilter(c)
-	return c:IsSetCard(0xc97) and c:IsAbleToRemoveAsCost()
-end
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.rfilter,tp,LOCATION_GRAVE,0,1,nil) end
-	Duel.Remove(Duel.SelectMatchingCard(tp,s.rfilter,tp,LOCATION_GRAVE,0,1,1,nil),POS_FACEUP,REASON_COST)
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetTargetParam(500)
-	Duel.SetTargetPlayer(tp)
-	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,500)
-end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-	Duel.Recover(p,d,REASON_EFFECT)
-end
-function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.Damage(tp,1000,REASON_COST)
-end
-function s.spfilter(c,e,tp)
-	return c:IsLevel(4) and c:IsSetCard(0x3c97) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc,e,tp) end
 	local c=e:GetHandler()
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsDestructable()
-		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,c,1,0,0)
 end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if Duel.Destroy(c,REASON_EFFECT)==0 then return end
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if ft<=0 then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	Duel.SpecialSummon(Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_HAND+LOCATION_GRAVE,0,1,math.min(ft,2),nil,e,tp),0,tp,tp,false,false,POS_FACEUP)
-end
-function s.cfilter(c)
-	return c:IsSetCard(0xac97) and c:IsType(TYPE_SPELL+TYPE_TRAP)
-end
-function s.actcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	Duel.Remove(Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil),POS_FACEUP,REASON_COST)
-end
-function s.rgtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,3,tp,LOCATION_GRAVE)
-end
-function s.rgfilter(c)
-	return c:IsSetCard(0xc97) and c:IsAbleToRemove()
-end
-function s.rgop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.GetMatchingGroup(s.rgfilter,tp,LOCATION_GRAVE,0,nil):SelectSubGroup(tp,aux.dncheck,false,3,3)
-	if g then Duel.Remove(g,POS_FACEUP,REASON_EFFECT) end
-end
-function s.repcfilter(c,e,val)
-	return c:IsAttackAbove(val) and not c:IsImmuneToEffect(e)
-end
-function s.rev(e,re,dam,r,rp,rc)
-	local g=Duel.GetMatchingGroup(aux.AND(Card.IsFaceup,Card.IsSummonType),tp,0,LOCATION_MZONE,nil,SUMMON_TYPE_SPECIAL)
-	local tg=g:Filter(s.repcfilter,nil,e,dam//2)
-	local rec=rc
-	if not rec and re then rec=re:GetHandler() end
-	local val=dam
-	Duel.DisableActionCheck(true)
-	if r&REASON_EFFECT>0 and #g>0 and #tg>0 then
-		for tc in aux.Next(tg) do
-			local e1=Effect.CreateEffect(e:GetHandler())
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-			e1:SetCode(EFFECT_UPDATE_ATTACK)
-			e1:SetValue(-dam//2)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-			tc:RegisterEffect(e1)
-		end
-		val=0
+	local tc=Duel.GetFirstTarget()
+	if c:IsRelateToChain() and tc:IsRelateToChain() and tc:IsFaceup() then
+		Duel.Equip(tp,c,tc)
 	end
-	Duel.DisableActionCheck(false)
-	return val
 end
-function s.rdtg(e,tp,eg,ep,ev,re,r,rp,chk)
+
+--E2
+function s.eqlimit(e,c)
+	return c:IsCode(CARD_VOIDICTATOR_DEMON_GUARDIAN_OF_CORVUS)
+end
+
+--E3
+function s.cfilter(c,_,tp)
+	return c:IsFaceup() and c:IsType(TYPE_RITUAL) and c:IsSummonPlayer(1-tp)
+end
+function s.checkfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_RITUAL) and not c:IsForbidden()
+end
+function s.eftg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,3,PLAYER_ALL,LOCATION_DECK)
+	Duel.SetTargetCard(eg)
 end
-function s.rdop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.DisableShuffleCheck()
-	Duel.Remove(Duel.GetDecktopGroup(tp,3)+Duel.GetDecktopGroup(1-tp,3),POS_FACEUP,REASON_EFFECT)
-end
-function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return not e:GetHandler():IsForbidden() end
-end
-function s.op(e,tp,eg,ep,ev,re,r,rp)
+function s.efop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsForbidden() or not c:IsRelateToEffect(e) then return end
-	local b=Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-		and aux.PandSSetCon(c,nil,c:GetLocation(),c:GetLocation())(nil,e,tp,eg,ep,ev,re,r,rp)
-	if b and Duel.SelectOption(tp,1159,1105)==0 then
-		aux.PandSSet(c,REASON_EFFECT)(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetTargetCards():Filter(s.checkfilter,nil)
+	if not c:IsRelateToChain() or c:IsFacedown() or #g<=0 then return end
+	local tc=g:GetFirst()
+	if #g>1 then
+		Duel.HintMessage(tp,HINTMSG_SELECT)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.HintSelection(sg)
+		tc=sg:GetFirst()
 	else
-		Duel.SelectOption(tp,1105)
-		aux.PandEnableFUInED(c,REASON_EFFECT)(e,tp,eg,ep,ev,re,r,rp)
+		Duel.HintSelection(Group.FromCards(tc))
 	end
+	if tc then
+		local code=tc:GetOriginalCode()
+		local cid=c:CopyEffect(code,RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END,2)
+	end
+end
+
+--E4
+function s.actcon(e)
+	return Duel.GetAttacker()==e:GetHandler() or Duel.GetAttackTarget()==e:GetHandler()
+end
+
+--E5
+function s.atkcon(e)
+	local c=e:GetHandler()
+	local bc=c:GetBattleTarget()
+	return Duel.GetCurrentPhase()==PHASE_DAMAGE_CAL and bc and bc:IsControler(1-e:GetHandlerPlayer())
+end
+function s.atkval(e,c)
+	local bc=e:GetHandler():GetBattleTarget()
+	local atk,def=bc:GetAttack(),bc:GetDefense()
+	if not bc:HasAttack() then atk=0 end
+	if not bc:HasDefense() then def=0 end
+	return math.max(atk,def)
+end
+
+--GE
+function s.grantcon(e)
+	local ec=e:GetHandler():GetEquipTarget()
+	return ec and s.filter(ec)
+end
+function s.grantfilter(e,c)
+	local ec=e:GetHandler():GetEquipTarget()
+	return ec and c==ec and s.filter(ec)
 end
